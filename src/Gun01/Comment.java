924d68a76911725f00414e544239e2fe1f9e3a4c
RoseKaya <gbuyruk@gmail.com>
2022-10-11 15:06:20 +0300

    dördüncü taskımı geri aldım

--- src/Gun01/Comment.java
+++ src/Gun01/Comment.java
@@ -4,11 +4,11 @@ public class Comment {
     public static void main(String[] args) {
 
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Merhaba"+i);
+        for (int i = 0; i < 5; i++ ) {
+            System.out.println("Merhaba "+i);
         }
 
-        System.out.println("selamlar");
+        System.out.println("selamlar ");
 
 
     }
