dc3dacf68ccd31c998e960aa100a1fcb416c32e1
RoseKaya <gbuyruk@gmail.com>
2022-10-11 14:59:00 +0300

    dördüncü taskımı bitirdim.

--- src/Gun01/Comment.java
+++ src/Gun01/Comment.java
@@ -2,13 +2,14 @@ package Gun01;
 
 public class Comment {
     public static void main(String[] args) {
-        System.out.println("Merhaba");
+
 
         for (int i = 0; i < 5; i++) {
             System.out.println("Merhaba"+i);
         }
 
         System.out.println("selamlar");
+        System.out.println("test1");
 
     }
 }
