b24729df218afd1b0338f06e9c2afd52ce12f277
RoseKaya <gbuyruk@gmail.com>
2022-10-11 14:45:41 +0300

    ikinci taskımı yaptım

--- src/Gun01/Comment.java
+++ src/Gun01/Comment.java
@@ -2,6 +2,11 @@ package Gun01;
 
 public class Comment {
     public static void main(String[] args) {
-        
+        System.out.println("Merhaba");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Merhaba"+i);
+        }
+
     }
 }
